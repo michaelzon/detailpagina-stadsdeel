614770e588d6f3f9a2b23729b5db8def70337b79
Michael Zonneveld <michael.zonneveld@live.nl>
2024-05-05 11:41:13 +0200

    toggle dropdown works

--- src/app/components/Dropdown.tsx
+++ src/app/components/Dropdown.tsx
@@ -3,6 +3,33 @@
 import React, { useState, createContext, useContext, ReactNode } from 'react';
 
 interface DropdownProps {
+    onSelect: (item: any) => void;
+    children: ReactNode;
+};
+
+interface ToggleProps {
+    // isOpen: boolean;
+    // toggle: () => void;
+};
+
+interface ListProps {
+    // highlightedIndex: number;
+    items: string[];
+};
+
+interface ItemProps {
+    item: string;
+    index?: number;
+    isHighlighted?: boolean;
+};
+
+interface DropdownComponent extends React.FC<DropdownProps> {
+    Toggle: React.FC<ToggleProps>;
+    List: React.FC<ListProps>;
+    Item: React.FC<ItemProps>;
+}
+
+interface DropdownContextProps {
     isOpen: boolean;
     toggle: () => void;
     highlightedIndex: number;
@@ -10,15 +37,24 @@ interface DropdownProps {
     items: string[];
     onSelect: (item: any) => void;
     handleSelect: (item: any) => void;
-    children?: ReactNode;
+    children: ReactNode;
 }
 
-const DropdownContext = createContext<DropdownProps>({ isOpen: false, toggle: () => { }, highlightedIndex: 0, setHighlightedIndex: () => { }, items: [''], onSelect: () => { }, handleSelect: () => { }, children: '' });
+const DropdownContext = createContext<DropdownContextProps>({
+    isOpen: false,
+    toggle: () => { },
+    highlightedIndex: 0,
+    setHighlightedIndex: () => { },
+    items: [''],
+    onSelect: () => { },
+    handleSelect: () => { },
+    children: ''
+});
 
-const Dropdown: React.FC<DropdownProps> = ({ children: any, onSelect }) => {
+const Dropdown: DropdownComponent = ({ children, onSelect }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [highlightedIndex, setHighlightedIndex] = useState(-1);
-    const [items, setItems] = useState(['option 1', 'option 2', 'option 3']);
+    const [items, setItems] = useState(['1', '2']);
 
     const toggle = () => setIsOpen(!isOpen);
     const close = () => setIsOpen(false);
@@ -34,10 +70,10 @@ const Dropdown: React.FC<DropdownProps> = ({ children: any, onSelect }) => {
         highlightedIndex,
         setHighlightedIndex,
         items,
-        onSelect,
         handleSelect,
+        onSelect, // weet niet zeker of dit klopt 
+        children // weet niet zeker of dit klopt
     };
-
 
     return (
         <DropdownContext.Provider value={contextValue}>
@@ -46,21 +82,45 @@ const Dropdown: React.FC<DropdownProps> = ({ children: any, onSelect }) => {
     )
 };
 
-// const 
+const Toggle: React.FC<ToggleProps> = () => {
+    const { isOpen, toggle } = useContext(DropdownContext);
 
+    return (
+        <button onClick={toggle} aria-haspopup="true" aria-expanded={isOpen}>
+            Toggle Dropdown
+        </button>
+    )
+};
 
+const List: React.FC = () => {
+    const { isOpen, highlightedIndex, items } = useContext(DropdownContext);
 
-// const Toggle = () => {
-//     const { isOpen, toggle } = useContext(DropdownContext)
+    return isOpen ? (
+        <ul>
+            {items.map((item, index) => (
+                <Dropdown.Item
+                    key={index}
+                    item={item}
+                    index={index}
+                    isHighlighted={index === highlightedIndex}
+                />
+            ))}
+        </ul>
+    ) : null
+};
 
-//     return (
-//         <button onClick={toggle} aria-haspopup="true" aria-expanded={isOpen}>
-//             Toggle Dropdown
-//         </button>
-//     )
-// }
+const Item: React.FC<ItemProps> = ({ item, isHighlighted }) => {
+    const { handleSelect,  } = useContext(DropdownContext)
+    return (
+        <li onClick={() => handleSelect(item)}>
+            {item + isHighlighted}
+        </li>
+    )
+}
 
-// Dropdown.Toggle = Toggle; 
+Dropdown.Toggle = Toggle;
+Dropdown.List = List;
+Dropdown.Item = Item;
 
 export default Dropdown;
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Card from "./components/Card";
+import Dropdown from "./components/Dropdown";
 
 interface Wijk {
   naam: string,
@@ -15,34 +16,89 @@ export default function Home() {
 
   const handleButtonClick = () => setCardIsOpen(!cardIsOpen);
 
-  useEffect(() => {
-    fetch('https://api.data.amsterdam.nl/v1/gebieden/stadsdelen/?naam=Nieuw-West')
-      .then(res => res.json())
-      .then(data => {
-        if (data && data['_embedded'] && data['_embedded']['stadsdelen'][0]) {
-          const id = data['_embedded']['stadsdelen'][0]['identificatie'];
-          return id;
-        } else {
-          throw new Error('No stadsdeel data found');
-        }
-      })
-      .then(identificatie => {
-        return fetch(`https://api.data.amsterdam.nl/v1/gebieden/wijken/?ligtInStadsdeel.identificatie=${identificatie}`);
-      })
-      .then(res => res.json())
-      .then(data => {
-        setWijken(data['_embedded'].wijken);
-      })
-      .catch(error => console.error('Error in fetching data:', error));
-  }, []);
+  const wijkenLijst = [
+    {
+      "identificatie": "03630970000040",
+      'naam': 'Sloterdijk Nieuw-West',
+    },
+    {
+      "identificatie": "03630970000041",
+      "naam": "Geuzenveld"
+    },
+    {
+      "identificatie": "03630970000042",
+      "volgnummer": 1,
+      "registratiedatum": "2024-01-25T20:36:51",
+      "naam": "Slotermeer-West",
+    },
+    {
+      "identificatie": "03630970000043",
+      "volgnummer": 1,
+      "registratiedatum": "2024-01-25T20:36:51",
+      "naam": "Slotermeer-Noordoost",
+    },
+    {
+      "identificatie": "03630970000044",
+      "volgnummer": 1,
+      "registratiedatum": "2024-01-25T20:36:51",
+      "naam": "Slotermeer-Zuidoost",
+    }
+  ];
+
+  const buurtenLijstGeuzenveld = [
+    {
+      "volgnummer": 1,
+      "registratiedatum": "2024-01-25T20:36:51",
+      "identificatie": "03630980000180",
+      "naam": "Osdorper Binnenpolder",
+    },
+    {
+      "volgnummer": 1,
+      "registratiedatum": "2024-01-25T20:36:51",
+      "identificatie": "03630980000181",
+      "naam": "De Eendracht",
+    },
+    {
+      "volgnummer": 1,
+      "registratiedatum": "2024-01-25T20:36:51",
+      "identificatie": "03630980000182",
+      "naam": "Ruys de Beerenbrouckbuurt",
+    },
+    {
+      "volgnummer": 1,
+      "registratiedatum": "2024-01-25T20:36:51",
+      "identificatie": "03630980000183",
+      "naam": "Eendrachtspark",
+    }
+  ];
+
+  // useEffect(() => {
+  //   fetch('https://api.data.amsterdam.nl/v1/gebieden/stadsdelen/?naam=Nieuw-West')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       if (data && data['_embedded'] && data['_embedded']['stadsdelen'][0]) {
+  //         const id = data['_embedded']['stadsdelen'][0]['identificatie'];
+  //         return id;
+  //       } else {
+  //         throw new Error('No stadsdeel data found');
+  //       }
+  //     })
+  //     .then(identificatie => {
+  //       return fetch(`https://api.data.amsterdam.nl/v1/gebieden/wijken/?ligtInStadsdeel.identificatie=${identificatie}`);
+  //     })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setWijken(data['_embedded'].wijken);
+  //     })
+  //     .catch(error => console.error('Error in fetching data:', error));
+  // }, []);
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
       </div>
       <div className={styles.center}>
-
-        {wijken.map((wijk: Wijk, i) => (
+        {wijkenLijst.map((wijk: Wijk, i) => (
           <div>
             {wijk.naam}
           </div>
@@ -51,14 +107,22 @@ export default function Home() {
       <div className={styles.grid}>
       </div>
       <button className={styles.simpleButton}
-      onClick={handleButtonClick}
-    >
-    {`card open? ${cardIsOpen}`}
-    </button>
+        onClick={handleButtonClick}
+      >
+        {`card open? ${cardIsOpen}`}
+      </button>
+      <Dropdown onSelect={() => { }}>
+        <Dropdown.Toggle />
+        <Dropdown.List>
+          {wijkenLijst.map((wijk: Wijk, i) => {
+            <Dropdown.Item item={wijk.naam} index={i}/>
+          })}
+        </Dropdown.List>
+      </Dropdown>
       <Card isOpen={cardIsOpen}>
+        <Card.Footer text='klap dicht' handleClose={handleButtonClick}></Card.Footer>
         <Card.Title title="some title"></Card.Title>
         <Card.Description description="some description"></Card.Description>
-        <Card.Footer text='klap dicht' handleClose={handleButtonClick}></Card.Footer>
       </Card>
     </main>
   );
